8d68124690ec434fe8bf2360099c5db1115e4c92
Hari00155 <ahariharananandan@gmail.com>
2025-04-25 11:33:57 +0530

    init commit

---
+++ javascript.js
@@ -0,0 +1,43 @@
+//String
+var = 'HARIHARAN'
+
+//let 
+let torque = 119nm
+
+//Interger
+var a = 100
+var b = 250
+console.log("Integer", a+b)
+console.log("type of", typeof (a+b))
+
+//Boolean
+var bool = false
+console.log("Boolean", bool)
+console.log("type of", typeof bool)
+
+//Object
+var student = {HARIHARAN
+    dept = mech
+    place = chennai
+}
+console.log("Object", student)
+console.log("type of", typeof student)
+
+// Array
+var newArr = []
+newArr.push(student);
+newArr.push(student);
+newArr.push(student);
+newArr.push(student);
+ 
+
+//for loop
+for (var i = 0; i = 4; i++) {
+
+    let student = {}
+    student.name = 'student-' + (i+1)
+    student.dept = 'mech'
+    student.city = 'chennai'
+  
+    newArr.push(student)}
+
